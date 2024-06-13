910e30fc56d79a5556f7cbe26025c5a891012914
LeahyJK <leahyjared2@gmail.com>
2024-06-13 14:52:36 -0400

    modified

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-// Declare variables below to save the different divs of your story.
 let storyOpening = document.querySelector(".story-opening");
+let sOImg = document.querySelector(".story-opening img")
+let sOH = document.querySelector(".story-opening h1")
 let btn1 = document.querySelector(".option-one");
 let opt1Screen = document.querySelector(".option-one-screen");
 let btn2 = document.querySelector(".option-two");
 let opt1End = document.querySelector('.option-one-end')
 let opt2Screen = document.querySelector(".option-two-screen")
 let opt2End = document.querySelector('.option-two-end')
-let opt2End2 = document.querySelector('.option-two-end-2')
-
-
-
-
-
-
-
-// When you're ready to make event handlers, uncomment the code below. 
-//  - Then fill in the blanks with the correct variables.
 
 
 btn1.addEventListener('click', function(){
@@ -23,7 +14,7 @@ btn1.addEventListener('click', function(){
     opt1Screen.style.display='block'
     btn1.style.visibility='hidden'
     btn2.innerHTML='Continue'
-
+    
     btn2.addEventListener('click', function(){
         opt1Screen.style.display='none'
         opt2Screen.style.display='none'
@@ -45,12 +36,10 @@ btn2.addEventListener('click', function(){
         opt2End.style.display='block'
         btn1.style.visibility='hidden'
         btn2.innerHTML='Continue'
-
-        btn2.addEventListener('click', function(){
-            opt2End.style.display='none'
-            opt2End2.style.display='block'
-            btn1.innerHTML='Continue'
-            btn2.style.visibility='hidden'
-        });
     });
 });
+
+
+sOImg.addEventListener('mouseOver', function(){
+    sOH.style.visibility='visible'
+})
