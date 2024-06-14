44972ab8bbbbb954553cc77dc1e932d44a476e12
LeahyJK <leahyjared2@gmail.com>
2024-06-14 14:41:30 -0400

    modified script

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let storyOpening = document.querySelector(".story-opening");
+let storyOpeningH1 = document.querySelector('.story-opening h1')
 let btn1 = document.querySelector(".option-one");
 let opt1Screen = document.querySelector(".option-one-screen");
 let btn2 = document.querySelector(".option-two");
@@ -7,10 +8,11 @@ let opt2Screen = document.querySelector(".option-two-screen")
 let opt2End = document.querySelector('.option-two-end')
 
 
+
 btn1.addEventListener('click', function(){
     storyOpening.style.display='none'
     opt1Screen.style.display='block'
-    btn1.style.visibility='hidden'
+    btn1.style.display='none'
     btn2.innerHTML='Continue'
     
     btn2.addEventListener('click', function(){
@@ -26,7 +28,7 @@ btn2.addEventListener('click', function(){
     storyOpening.style.display='none'
     opt2Screen.style.display='block'
     btn1.innerHTML='Continue'
-    btn2.style.visibility='hidden'
+    btn2.style.display='none'
 
     btn1.addEventListener('click', function(){
         opt1Screen.style.display='none'
@@ -34,5 +36,25 @@ btn2.addEventListener('click', function(){
         opt2End.style.display='block'
         btn1.style.visibility='hidden'
         btn2.innerHTML='Continue'
+    });
+});
+
+btn2.addEventListener("mouseover", function(){
+    storyOpening.style.backgroundColor='green'
+    storyOpeningH1.style.color='black'
+
+    btn2.addEventListener("mouseout", function(){
+        storyOpening.style.backgroundColor='black'
+        storyOpeningH1.style.color='white'
+    });
+});
+
+btn1.addEventListener("mouseover", function(){
+    storyOpening.style.backgroundColor='white'
+    storyOpeningH1.style.color='black'
+
+    btn1.addEventListener("mouseout", function(){
+        storyOpening.style.backgroundColor='black'
+        storyOpeningH1.style.color='white'
     });
 });
